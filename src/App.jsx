8c96048cefc8537b952e106ce50530eabfd40721
DoThanhDuB2111834 <dub2111834@student.ctu.edu.vn>
2025-04-26 09:38:29 +0700

    Install mui

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Button from '@mui/material/Button';
+import * as React from 'react';
+import Stack from '@mui/material/Stack';
+import { green } from '@mui/material/colors';
+import Icon from '@mui/material/Icon';
 
 function App() {
-  const [count, setCount] = useState(0)
+
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
+      <Stack direction="row" spacing={3}>
+        <Icon>add_circle</Icon>
+        <Icon color="primary">add_circle</Icon>
+        <Icon sx={{ color: green[500] }}>add_circle</Icon>
+        <Icon fontSize="small">add_circle</Icon>
+        <Icon sx={{ fontSize: 30 }}>add_circle</Icon>
+      </Stack>
     </>
   )
 }
