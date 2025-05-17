2b6b787ff6a9b47a31468943ecdfd8fedcd2220f
DoThanhDuB2111834 <dub2111834@student.ctu.edu.vn>
2025-05-17 19:58:14 +0700

    create new column and card

--- src/pages/Boards/_id.jsx
+++ src/pages/Boards/_id.jsx
@@ -1,13 +1,16 @@
 import * as React from 'react';
 import Container from '@mui/material/Container';
+import { isEmpty } from 'lodash';
+
 
 import AppBar from '~/components/AppBar/AppBar';
 import BoardBar from './BoardBar/BoardBar';
 import BoardContent from './BoardContent/BoardContent';
+import { generatePlaceholderCard } from '~/utils/formatters';
 
-import { mockData } from '~/apis/mock-data';
-
-import { fetchBoardDetailsAPI, createNewCardAPI, createNewColumnAPI } from '~/apis/index';
+import {
+  fetchBoardDetailsAPI, createNewCardAPI, createNewColumnAPI
+} from '~/apis/index';
 
 function Board() {
   const [board, setBoard] = React.useState(null);
@@ -16,6 +19,13 @@ function Board() {
     const boardId = '6820b9391b72625d8dce2a5b';
     fetchBoardDetailsAPI(boardId)
       .then(board => {
+        board.columns.forEach(column => {
+          // Cần xử lý vấn đề kéo thả vào 1 column rỗng
+          if (isEmpty(column.cards)) {
+            column.cards= [generatePlaceholderCard(column)]
+            column.cardOrderIds = [generatePlaceholderCard(column)._id]
+          }
+        })
         setBoard(board);
       })
       .catch((error) => {
@@ -28,6 +38,15 @@ function Board() {
       ...newColumnData,
       boardId: board._id
     })
+
+    // Khi mới tạo column thì chưa có card, cần xử lý vấn đề kéo thả bằng vào column mới bằng cách để 1 placeholder card
+    createdColumn.cards = [generatePlaceholderCard(createdColumn)]
+    createdColumn.cardOrderIds = [generatePlaceholderCard(createdColumn)._id]
+
+    const newBoard = { ...board }
+    newBoard.columns.push(createdColumn)
+    newBoard.columnOrderIds.push(createdColumn._id)
+    setBoard(newBoard)
   }
 
   const createNewCard = async (newCardData) => {
@@ -35,6 +54,14 @@ function Board() {
       ...newCardData,
       boardId: board._id
     })
+
+    const newBoard = { ...board }
+    const columnToUpdate = newBoard.columns.find(column => column._id === createdCard.columnId)
+    if (columnToUpdate) {
+      columnToUpdate.cards.push(createdCard)
+      columnToUpdate.cardOrderIds.push(createdCard._id)
+    }
+    setBoard(newBoard)
   }
 
   return (
